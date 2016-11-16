3a3dde9f51be88a4a77fc709e86eff1c06f5932e
Brian Aguirre <brianaguirre@Brians-MacBook-Pro-2.local>
2016-11-16 10:11:11 -0500

    AcceleratedC++ directory, HelloWorld App

---
+++ AcceleratedC++/Chapter0/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Brian Aguirre on 11/14/16.
+//  Copyright © 2016 Brian Aguirre. All rights reserved.
+//
+//  A small C++ program.
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello, World!\n" << std::endl;
+    
+    return 0;
+}
