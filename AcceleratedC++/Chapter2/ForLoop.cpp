d5ef5b675695ded20bf3a6d0f009a23fc289b9ba
Brian Aguirre <brian.n.aguirre@gmail.com>
2016-11-17 23:13:38 -0500

    Dictoery: Chapter2, Simple For Loop to count to 10

---
+++ AcceleratedC++/Chapter2/ForLoop.cpp
@@ -0,0 +1,27 @@
+//
+// ForLoop.cpp
+// Chapter2
+//
+// Created by Brian Aguirre on 11/17/2016.
+// Copyright © Brian Aguirre. All rights reserved.
+// 
+// Creates a loop to count to 10.
+
+
+#include <iostream>
+
+
+int main(){
+
+  for (int i=1; i<=10; i++){
+
+    std::cout << i << " ";
+    
+  }
+
+  
+
+
+
+  return 0;
+}
